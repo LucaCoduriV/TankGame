add17960ef0a8f1bf6630a886b2d38dc498a0baf
LucaCoduriV <luca.coduri@cpnv.ch>
2019-11-26 14:15:30 +0100

    Add SpawnCreator + SpawnController

---
+++ TankGame/Assets/scripts/SpawnController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    List<Vector2> spawnList;
+
+    private void Start()
+    {
+        spawnList = SpawnCreator.instance.getSpawns();
+        foreach (Vector2 spawn in spawnList)
+        {
+            Debug.Log(spawn.x + ";" + spawn.y);
+        }
+    }
+}
--- TankGame/Assets/scripts/SpawnCreator.cs
+++ TankGame/Assets/scripts/SpawnCreator.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class SpawnCreator : MonoBehaviour
 {
+    public List<Vector2> spawnList = new List<Vector2>();
+    public static SpawnCreator instance = null;
+    public float offSet = 1;
+
+    private float meshWidth;
     private Vector2[] colliderPath;
 
-    public float offSet = 1;
 
     private void Awake()
     {
- 
+        if(instance == null)
+        {
+            instance = this;
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 position = new Vector2(2, GetHeightForX(2));
-        Debug.Log(position.y);
-        position.y += offSet;
-
-        GameObject.Find("Tank").transform.position = position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    float GetHeightForX(float x)
-    {
-
         colliderPath = GetComponent<PolygonCollider2D>().GetPath(0);
+        meshWidth = GetMeshWidth();
+        spawnList = getSpawns();
+    }
+
+    
+    //Permet d'avoir la taille du mesh
+    public float GetMeshWidth()
+    {
+        float max = 0;
+
+        foreach (Vector2 vector in colliderPath)
+        {
+            max = vector.x > max ? vector.x : max;
+        }
+
+        return max + 1;
+    }
+
+    //permet d'avoir le Y en fonction de X;
+    public float GetHeightForX(float x)
+    {
         foreach (Vector2 vector in colliderPath)
         {
             if(vector.x == x && vector.y != 0)
@@ -43,6 +58,19 @@ public class SpawnCreator : MonoBehaviour
         }
 
         return 0;
+    }
+    //permet d'avoir la liste des spawn disponible pour le mesh
+    public List<Vector2> getSpawns()
+    {
+        List<Vector2> spawnList = new List<Vector2>();
+
+
+        for (int i = 0; i < meshWidth; i++)
+        {
+            spawnList.Add(new Vector2(i, GetHeightForX(i) + offSet));
+        }
+
+        return spawnList;
     }
 
 }
